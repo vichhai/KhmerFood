7da07be38475b4bf5937cba3a945344028d97ec7
vichhai <kanvichhai@gmail.com>
2015-12-01 16:48:37 +0900

    Initial Commit

---
+++ KhmerFoodTests/KhmerFoodTests.m
@@ -0,0 +1,39 @@
+//
+//  KhmerFoodTests.m
+//  KhmerFoodTests
+//
+//  Created by kvc on 12/1/15.
+//  Copyright © 2015 Donut. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KhmerFoodTests : XCTestCase
+
+@end
+
+@implementation KhmerFoodTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
